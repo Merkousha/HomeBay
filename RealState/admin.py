385ff745e3f59561e08c7a46b16038c0392314df
Merko <Merko@LAPTOP-3P627PH6>
2023-10-08 19:58:28 +0330

    first commit

---
+++ RealState/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from  RealState.models import Country,State,City,Area ,Property , Property_Images
+
+
+
+class PropertyImagesInline(admin.TabularInline):
+    model = Property_Images
+    extra = 10
+
+class PropertyModelADmin(admin.ModelAdmin):
+    inlines= [PropertyImagesInline]
+
+
+# Register your models here.
+
+admin.site.register(Country)
+admin.site.register(State)
+admin.site.register(City)
+admin.site.register(Area)
+admin.site.register(Property , PropertyModelADmin)
+admin.site.register(Property_Images)
+
---
+++ RealState/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+
+# Create your models here.
+
+class Country(models.Model):
+    name = models.CharField(max_length=100)    
+   
+class State(models.Model):
+    name = models.CharField(max_length=100)     
+    country = models.ForeignKey(Country,on_delete=models.CASCADE)    
+    
+class City(models.Model):
+    name = models.CharField(max_length=100)
+    state = models.ForeignKey(State,on_delete=models.CASCADE)        
+
+class Area(models.Model):
+    name = models.CharField(max_length=100)
+    city = models.ForeignKey(City,on_delete=models.CASCADE)    
+    
+class Property(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField( max_length=2000)
+    address = models.TextField(max_length=300)
+    phone_number = models.CharField(max_length=10)
+    Year_of_Construction= models.DecimalField(max_digits=4,decimal_places=0)
+    Number_of_rooms= models.DecimalField(max_digits=2 ,decimal_places=0)
+    Ground_Area=models.FloatField()
+    Features=models.TextField(max_length=4000)
+    Total_Price=models.DecimalField(max_digits=14,decimal_places=0)
+    Price_per_meter=models.DecimalField(max_digits=14,decimal_places=0)
+    area = models.ForeignKey(Area,on_delete=models.CASCADE)    
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['id']    
+        
+class Property_Images(models.Model):
+    picture = models.ImageField(null=True, blank=True)
+    property = models.ForeignKey(Property,on_delete=models.CASCADE)    
+    
+    
