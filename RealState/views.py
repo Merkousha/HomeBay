1f803eba8d71a257aac340e90693d6fc03e2fb52
abolfazl-shekarpour <Abolfazlshekarpour123@gmail.com>
2023-10-17 18:58:43 +0400

    list

--- RealState/views.py
+++ RealState/views.py
@@ -1,3 +1,22 @@
+import random
+from datetime import datetime, timedelta
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, 'list.html')
+
+def contact(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+
+def events(request):
+    event = {
+        'title': 'Conference',
+        'description': 'Join us for an insightful conference on the latest industry trends.',
+        'image': 'https://example.com/conference.jpg'
+    }
+    context = {'event': event}
+    return render(request, 'list.html', context)
