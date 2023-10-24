5b3d4dfa83d188f3b637dd529edf178bca340543
Merko <Merko@LAPTOP-3P627PH6>
2023-10-24 19:40:53 +0330

    create list and detail page for properties

---
+++ RealState/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path , include
+from django.contrib.auth import views as auth_views
+from django.views.generic import RedirectView
+from django.conf.urls.static import static 
+from django.conf import settings  
+from django.contrib import admin
+from .views import list,detail
+
+
+urlpatterns = [
+    path('', list , name='list'),
+    path('detail/<int:pk>/', detail , name='detail'),
+]
--- RealState/views.py
+++ RealState/views.py
@@ -1,10 +1,17 @@
-import random
+from django.shortcuts import render, get_object_or_404, redirect
 from datetime import datetime, timedelta
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Property
 
-def index(request):
-    return render(request, 'list.html')
+def list(request):
+    properties = Property.objects.all() #Read All Properties from Database
+    return render(request, 'list.html',{'properties': properties})
+
+def detail(request,pk=None):
+    property = get_object_or_404(Property , pk=pk)
+    return render(request, 'detail.html',{'property': property})
+    
 
 def contact(request):
     if request.method == 'POST':
@@ -12,11 +19,3 @@ def contact(request):
         email = request.POST.get('email')
         message = request.POST.get('message')
 
-def events(request):
-    event = {
-        'title': 'Conference',
-        'description': 'Join us for an insightful conference on the latest industry trends.',
-        'image': 'https://example.com/conference.jpg'
-    }
-    context = {'event': event}
-    return render(request, 'list.html', context)
